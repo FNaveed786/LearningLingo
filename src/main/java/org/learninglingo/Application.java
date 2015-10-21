ac6039615007b855bc04a58ffc35b70b4cb1dd9b
Daniel D'Souza <daniel.dsouza5@gmail.com>
2015-10-20 23:35:22 -0700

    first commit

---
+++ src/main/java/org/learninglingo/Application.java
@@ -0,0 +1,26 @@
+package org.learninglingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by daniel on 9/22/15.
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
+
+
